505761481b6c937368b0b67310200e7bbd574e08
Kase <onuohakelechigreat@gmail.com>
2024-11-06 17:50:18 +0000

    new commit

--- gitlearning.py
+++ gitlearning.py
@@ -1,7 +1,6 @@
 def home_shopping():
     item = ['Pot', 'Kettle', 'gas cooker', 'Blender', 'Oven', 'Microwave']
     print("Attendant: Welcome to our sore")
-    Attendant = input('What would you like to buy: ')
-    print('Happy customer it is great to have you buy from our store')
+    print(item)
 
 home_shopping()
